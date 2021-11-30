66674fb2e90400cb2c6425238a8cc22b474057cb
John Bae <john21225@gmail.com>
2021-11-29 19:28:17 -0800

    Created pool (proxy server) for postgres database

--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const pool = require("./db");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+// create todo
+// get all todos
+// get todo by id
+// update todo
+// delete todo
 
 app.listen(5000, () => {
 	console.log("Server running on: http://localhost:5000");
