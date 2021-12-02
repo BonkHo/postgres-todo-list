65437ec03bd14dbd11d6c1bdb00a5b6ffecf3beb
John Bae <john21225@gmail.com>
2021-12-01 16:40:23 -0800

    config file contains password

--- server/db.js
+++ server/db.js
@@ -1,8 +1,9 @@
 const Pool = require("pg").Pool;
+const { PASSWORD } = require("./config");
 
 const pool = new Pool({
 	user: "postgres",
-	password: "password",
+	password: PASSWORD,
 	host: "localhost",
 	post: 5432,
 	database: "perntodo",
