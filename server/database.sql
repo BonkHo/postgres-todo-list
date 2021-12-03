d7e9b8382bd1d5af9ed8136a170539dcd179baec
John Bae <john21225@gmail.com>
2021-12-02 19:24:31 -0800

    Initialized client react app

--- server/database.sql
+++ server/database.sql
@@ -1,4 +1,9 @@
+-- Commands for the database
+
 CREATE DATABASE perntodo;
+
+-- Creates user table with id and description.
+-- SERIAL function is used to auto increment the id.
 
 CREATE TABLE todo(
   todo_id SERIAL PRIMARY KEY,
